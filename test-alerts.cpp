6a9d3835fc2e88ddf1fea7f2ef42b8f4d03c995e
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-02-21 11:11:38 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,5 +4,10 @@
 #include "typewise-alert.h"
 
 TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(PASSIVE_COOLING, -20)== TOO_LOW);
+  REQUIRE(inferBreach(PASSIVE_COOLING, 45)== TOO_HIGH);
+  REQUIRE(inferBreach(HI_ACTIVE_COOLING, -20)== TOO_LOW);
+  REQUIRE(inferBreach(HI_ACTIVE_COOLING, 50)== TOO_LOW);
+  REQUIRE(inferBreach(MED_ACTIVE_COOLING, -20)== TOO_LOW);
+  REQUIRE(inferBreach(MED_ACTIVE_COOLING, 45)== TOO_LOW);
 }

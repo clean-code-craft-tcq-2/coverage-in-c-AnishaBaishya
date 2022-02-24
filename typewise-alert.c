da8aedb8ad51aff158f5b2927936e17031c92dd0
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-02-24 17:36:37 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,56 +1,35 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
+TempConfig TemperatureConfiguration[NUMBEROF_COOLING_TYPES]={
+  {PASSIVECOOLING_LOWERLIMIT,PASSIVECOOLING_UPPERLIMIT},
+  {HI_ACTIVECOOLING_LOWERLIMIT,HI_ACTIVECOOLING_UPPERLIMIT},
+  {MED_ACTIVECOOLING_LOWERLIMIT,MED_ACTIVECOOLING_UPPERLIMIT}
+};
+
 void (*AlertTarget_fp[NUMBEROF_ALERT_TARGETS])(BreachType)={sendToController,sendToEmail};
 
 const char* breachAlertMessage[] = {"Hi, the temperature is Normal","Hi, the temperature is too low", "Hi, the temperature is too high"};
 
-TempConfig setLimitsAccordingToCoolingType(double lowLimit, double highLimit)
-{
-	TempConfig TempLimit;
-	TempLimit.LowerLimit = lowLimit;
-	TempLimit.UpperLimit = highLimit;
-	return TempLimit;
-}
-
-TempConfig PASSIVE_COOLING()
-{
-	return setLimitsAccordingToCoolingType(PASSIVECOOLING_LOWERLIMIT,PASSIVECOOLING_UPPERLIMIT);
-}
-
-TempConfig HI_ACTIVE_COOLING()
-{
-	return setLimitsAccordingToCoolingType(HI_ACTIVECOOLING_LOWERLIMIT,HI_ACTIVECOOLING_UPPERLIMIT);
-}
-
-TempConfig MED_ACTIVE_COOLING()
-{
-	return setLimitsAccordingToCoolingType(MED_ACTIVECOOLING_LOWERLIMIT,MED_ACTIVECOOLING_UPPERLIMIT);
-}
-
-BreachType inferBreach(TempConfig TempConfigCoolingType, double TempValue) {
+BreachType inferBreach(CoolingType TypeOfCooling, double TempValue) {
   BreachType returnbreachtype = NORMAL;
-  if(TempValue < TempConfigCoolingType.LowerLimit) {
+  if(TempValue < TemperatureConfiguration[TypeOfCooling].LowerLimit) {
     returnbreachtype= TOO_LOW;
   }
-  if(TempValue > TempConfigCoolingType.UpperLimit) {
+  if(TempValue > TemperatureConfiguration[TypeOfCooling].UpperLimit) {
     returnbreachtype = TOO_HIGH;
   }
   return returnbreachtype;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  TempConfig TempConfigCoolingType;
-  TempConfigCoolingType = ClassifyCoolingType();
-  return inferBreach(TempConfigCoolingType,temperatureInC);
-}
-
-void checkAndAlert(
+BreachType  checkAndAlert(
     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
 
-  BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+  BreachType breachType = inferBreach(
+    batteryChar.coolingType, temperatureInC
+  );
   AlertTarget_fp[alertTarget](breachType);
+  return breachType;
 }
 
 void sendToController(BreachType breachType) {

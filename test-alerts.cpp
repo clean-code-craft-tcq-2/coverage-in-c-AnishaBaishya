95ecc1c5c3293b878cc90ab25f9870e50afe51e3
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-02-24 17:07:28 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -2,28 +2,3 @@
 
 #include "test/catch.hpp"
 #include "typewise-alert.h"
-
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(PASSIVE_COOLING, -20)== TOO_LOW);
-  REQUIRE(inferBreach(PASSIVE_COOLING, 45)== TOO_HIGH);
-  REQUIRE(inferBreach(HI_ACTIVE_COOLING, -20)== TOO_LOW);
-  REQUIRE(inferBreach(HI_ACTIVE_COOLING, 50)== TOO_HIGH);
-  REQUIRE(inferBreach(MED_ACTIVE_COOLING, -20)== TOO_LOW);
-  REQUIRE(inferBreach(MED_ACTIVE_COOLING, 45)== TOO_HIGH);
-}
-
-TEST_CASE("checks the main function Alert Target CONTROLLER") {
-  AlertTarget alertTarget = TO_CONTROLLER;
-  CoolingType coolingtype = MED_ACTIVE_COOLING;
-  BatteryCharacter batteryChar = {coolingtype, "BrandX"};
-  REQUIRE(checkAndAlert(alertTarget, batteryChar, 45) == TOO_HIGH);
-  REQUIRE(checkAndAlert(alertTarget, batteryChar, -5) == TOO_LOW);
-}
-
-TEST_CASE("checks the main function Alert Target EMAIL") {
-  AlertTarget alertTarget = TO_EMAIL;
-  CoolingType coolingtype = HI_ACTIVE_COOLING;
-  BatteryCharacter batteryChar = {coolingtype, "BrandX"};
-  REQUIRE(checkAndAlert(alertTarget, batteryChar, -10) == TOO_LOW);
-  REQUIRE(checkAndAlert(alertTarget, batteryChar, 55) == TOO_HIGH);
-}
